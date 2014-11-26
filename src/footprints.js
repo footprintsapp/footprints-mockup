27766b4b79f43244a06a73c7e07de29bf7123ca2
Christopher Su <christophersu9@gmail.com>
2014-11-26 10:28:57 -0800

    rows

--- src/footprints.js
+++ src/footprints.js
@@ -21,7 +21,7 @@ $(function() {
         return array;
     }
 
-    var locations = shuffleArray(["Seattle, WA", "New York, NY", "Boston, MA", "San Francisco, CA", "San Diego, CA", "Los Angeles, CA"]);
+    var locations = ["July 2014", "August 2014", "September 2014", "October 2014", "November 2014"];
 
     locations.forEach(function(loc) {
         $('.fp-gallery-inject').append('<h3 class="fp-section-heading">' + loc + '</h3><div class="fp-gallery-section"></div>');
@@ -31,10 +31,17 @@ $(function() {
         for (var i = 0; i < getRandomInt(2, 5); i++) {
             $(this).append('<div class="row fp-gallery-row"></div>');
         }
+        $(this).append('<div class="row fp-gallery-row-partial"></div>');
     });
 
     $('.fp-gallery-row').each(function() {
         for (var i = 0; i < 12; i++) {
+            $(this).append('<div class="col-md-1 fp-gallery-image"><img src="assets/img/placeholder-100x100.gif" width="100%" height="100%" /></div>');
+        }
+    });
+    
+    $('.fp-gallery-row-partial').each(function() {
+        for (var i = 0; i < getRandomInt(3, 12); i++) {
             $(this).append('<div class="col-md-1 fp-gallery-image"><img src="assets/img/placeholder-100x100.gif" width="100%" height="100%" /></div>');
         }
     });
