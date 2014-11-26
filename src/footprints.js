ff900a56fe85e720e6cf14d9b5a4717f848c0d5e
Christopher Su <christophersu9@gmail.com>
2014-11-26 01:25:09 -0800

    loop

--- src/footprints.js
+++ src/footprints.js
@@ -3,7 +3,7 @@ $(function() {
     var galleryImage = $("div").addClass("col-md-1 fp-gallery-image");
     galleryImage.append($("img").attr("src", "http://placehold.it/100x100").attr("width", "100%").attr("height", "100%"));
 
-    for (int i = 0; i < 12; i++) {
+    for (var i = 0; i < 12; i++) {
         gallery.append(galleryImage);
     }
 
