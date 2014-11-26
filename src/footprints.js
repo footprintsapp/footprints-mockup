e1c4584d223aae6be9539983e879e0d12edf0d30
Christopher Su <christophersu9@gmail.com>
2014-11-26 01:35:48 -0800

    img

--- src/footprints.js
+++ src/footprints.js
@@ -7,7 +7,7 @@ $(function() {
 
     $('.fp-gallery-row').each(function() {
         for (var i = 0; i < 12; i++) {
-            $(this).append('<div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>');
+            $(this).append('<div class="col-md-1 fp-gallery-image"><img src="assets/img/placeholder-100x100.gif" width="100%" height="100%" /></div>');
         }
     });
 });
