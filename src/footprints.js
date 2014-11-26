5098b87b22393517be4bc546e849caef01d1261d
Christopher Su <christophersu9@gmail.com>
2014-11-26 01:34:48 -0800

    functional

--- src/footprints.js
+++ src/footprints.js
@@ -1,13 +1,13 @@
 $(function() {
-    var gallery = $("div").addClass("row");
-    var galleryImage = $("div").addClass("col-md-1 fp-gallery-image");
-    galleryImage.append($("img").attr("src", "http://placehold.it/100x100").attr("width", "100%").attr("height", "100%"));
-
-    for (var i = 0; i < 12; i++) {
-        gallery.append(galleryImage);
-    }
-
     $('.fp-gallery-section').each(function() {
-        $(this).append(gallery);
+        for (var i = 0; i < 4; i++) {
+            $(this).append('<div class="row fp-gallery-row"></div>');
+        }
+    });
+
+    $('.fp-gallery-row').each(function() {
+        for (var i = 0; i < 12; i++) {
+            $(this).append('<div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>');
+        }
     });
 });
