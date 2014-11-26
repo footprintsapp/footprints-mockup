3c547f43c7f54ac477ba54d61ebf3931eb604d6e
Christopher Su <christophersu9@gmail.com>
2014-11-26 01:21:06 -0800

    jquery

---
+++ src/footprints.js
@@ -0,0 +1,74 @@
+$(function() {
+    $('.fp-gallery-section').each(function() {
+        $(this).append('<div class="row">
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+      </div>
+      <div class="row">
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+      </div>
+      <div class="row">
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+      </div>
+      <div class="row">
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+      </div>
+      <div class="row">
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+        <div class="col-md-1 fp-gallery-image"><img src="http://placehold.it/100x100" width="100%" height="100%" /></div>
+      </div>')
+    });
+});
