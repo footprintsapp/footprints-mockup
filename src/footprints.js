b90b23bb80186575c5b09553dd11ce710d59ca0f
Christopher Su <christophersu9@gmail.com>
2014-12-02 12:05:48 -0800

    lightbox

--- src/footprints.js
+++ src/footprints.js
@@ -8,18 +8,32 @@ $(function() {
     var locations = ["July 2014", "August 2014", "September 2014", "October 2014", "November 2014"];
 
     var getNextImage = function() {
+        var imageObject = {};
+
         if (globalImageCounter < 80) {
-            return 'http://footprintsapp.github.io/footprints-img/mockup-scaled/JPEG/' + globalImageCounter++ + '.jpg';
+            imageObject.thumb = 'http://footprintsapp.github.io/footprints-img/mockup-scaled/JPEG/' + globalImageCounter++ + '.jpg';
+            imageObject.full = 'http://footprintsapp.github.io/footprints-img/mockup/' + globalImageCounter++ + '.JPG';
         }
         else {
-            return "assets/img/placeholder-100x100.gif";
+            imageObject.thumb = "assets/img/placeholder-100x100.gif";
+            imageObject.full = "assets/img/placeholder-100x100.gif";
         }
+
+        return imageObject;
     }
 
     var nextThumbnail = function() {
+        var image = getNextImage();
+
+        var imageLightbox = $(document.createElement('a'));
+        imageLightbox.attr('href', image.full);
+        imageLightbox.attr('data-lightbox', 'all-group');
+
         var nextThumbnailElement = $(document.createElement('div')).addClass("square fp-gallery-image");
-        nextThumbnailElement.css('background-image', 'url(' + getNextImage() + ')');
-        return nextThumbnailElement;
+        nextThumbnailElement.css('background-image', 'url(' + image.thumb + ')');
+
+        imageLightbox.append(nextThumbnailElement);
+        return imageLightbox;
     }
 
     locations.forEach(function(loc) {
